fbce9d76257e9e63d88ddff72ef081a608676b83
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-30 20:06:34 +0530

    feat: display number of players and prize money

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useEffect, useState } from "react";
 import lottery from "./lottery";
+import web3 from "./web3";
 
 const App = () => {
   const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       const fetchedManager = await lottery.methods.manager().call();
       setManager(fetchedManager);
+
+      const fetchedPlayers = await lottery.methods.getPlayers().call();
+      setPlayers(fetchedPlayers);
+
+      const fetchedBalance = await web3.eth.getBalance(lottery.options.address);
+      setBalance(fetchedBalance);
     };
     fetchData();
   }, []);
@@ -15,7 +24,11 @@ const App = () => {
   return (
     <div>
       <h2>Lottery Contract</h2>
-      <p>This contract is managed by {manager}</p>
+      <p>This contract is managed by {manager}.</p>
+      <p>
+        There are currently {players.length} people entered, competing to win{" "}
+        {web3.utils.fromWei(balance)} ether.
+      </p>
     </div>
   );
 };
